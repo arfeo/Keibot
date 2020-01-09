6c8190ce6041cc571a9509d9324547e34b116f6f
Leonid Belikov <mail@arfeo.net>
2020-01-09 20:36:56 +0300

    game: helpers: + getMapItemsByType & getRandomNum functions

--- src/components/Game/helpers.ts
+++ src/components/Game/helpers.ts
@@ -17,4 +17,52 @@ function setCellSize(vmin: number): number {
   return Math.round(calculateVMin() * vmin  / 10) * 10;
 }
 
-export { setCellSize };
+/**
+ * Function returns an array of items' coordinates for the given board map
+ * according to the given item type; if the given map is undefined or not an array,
+ * function returns an empty array
+ *
+ * @param map
+ * @param type
+ */
+function getMapItemsByType(map: number[][], type: number): number[][] {
+  const result: number[][] = [];
+
+  if (!map || !Array.isArray(map)) {
+    return result;
+  }
+
+  for (let y = 0; y < map.length; y += 1) {
+    for (let x = 0; x < map[y].length; x += 1) {
+      if (map[y][x] === type) {
+        result.push([y, x]);
+      }
+    }
+  }
+
+  return result;
+}
+
+/**
+ * Returns a random number in a given interval; as an option it discards one or more numbers given
+ * in the `discard` array
+ *
+ * @param min
+ * @param max
+ * @param discard
+ */
+function getRandomNum(min = 1, max = 1000, discard: number[] = []): number {
+  const num: number = Math.floor(min + Math.random() * (max + 1 - min));
+
+  if (discard.indexOf(num) > -1) {
+    return getRandomNum(min, max, discard);
+  }
+
+  return num;
+}
+
+export {
+  getMapItemsByType,
+  getRandomNum,
+  setCellSize,
+};
