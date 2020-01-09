055b9b42ba525a6060866376abae7354e08f7600
Leonid Belikov <mail@arfeo.net>
2020-01-09 03:03:16 +0300

    Menu component update (+ helpMessage)

--- src/components/Menu/index.ts
+++ src/components/Menu/index.ts
@@ -1,11 +1,34 @@
 import { MenuComponent } from '../core/Menu';
 import { Game } from '../Game';
+import { Alert } from '../common/Alert';
 
 import { APP } from '../../constants/game';
 
 export class Menu extends MenuComponent {
+  protected helpMessage: string;
+
   public init(): void {
     this.appRoot = document.getElementById('root');
+
+    this.helpMessage = (`
+      There are 3 ways to win Keibot (pronounced Key-bo):
+
+        1.  Get 3 beads in a row (horizontally, vertically, or diagonally)
+        2.  Capture 3 of your opponent's statues
+        3.  Place all ten of your beads on the board
+      (Actually, there is a fourth way to win, but it happens very rarely — trap your 
+      opponent so he can't move.)
+      
+      The statues move like knights in chess (an L-shaped move, two squares 
+      horizontally or vertically and then one square perpendicularly).  To move a piece, 
+      click on its square, then on the destination square.
+      
+      Place your beads by aligning yourself with your opponent, with one square 
+      in between — a bead goes in that square.
+      
+      Capture your opponent's statues by landing on them (except for the 
+      last to move — he's safe.  He's the one with a shield).
+    `);
 
     this.items = [
       {
@@ -44,7 +67,7 @@ export class Menu extends MenuComponent {
         action: {
           type: 'click',
           handler: () => {
-            console.log('Help');
+            new Alert(this, this.helpMessage, 'large');
           },
         },
       },
