d536efd7fa2f85dafbb715a79625195d5f225824
Leonid Belikov <mail@arfeo.net>
2020-03-01 20:00:28 +0300

    game: render update

--- src/components/Game/render.ts
+++ src/components/Game/render.ts
@@ -92,7 +92,7 @@ function renderGridCell(x: number, y: number): void {
   for (let i = 0; i <= hmTimes; i += 1) {
     const randomX = Math.floor((Math.random() * this.cellSize) + 1);
     const randomY = Math.floor((Math.random() * this.cellSize) + 1);
-    const randomSize = Math.floor((Math.random() * 1.3) + 1);
+    const randomSize = Math.floor((Math.random() * 1.3 * this.cellSize / 75) + 1);
     const randomOpacityOne = Math.floor((Math.random() * 9) + 1);
     const randomOpacityTwo = Math.floor((Math.random() * 9) + 1);
 
