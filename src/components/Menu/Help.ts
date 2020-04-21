ef5ebab6155c084e983d904c855104cfb3c3dcec
Leonid Belikov <mail@arfeo.net>
2020-04-21 03:44:31 +0300

    Menu component update (Help)

--- src/components/Menu/Help.ts
+++ src/components/Menu/Help.ts
@@ -23,30 +23,33 @@ class Help extends ModalComponent {
   public render(): HTMLElement {
     const labelText = document.createElement('div');
 
-    labelText.innerText = (
-      `There are 3 ways to win Keibot (pronounced Key-bo):
+    labelText.innerHTML = (`
+      <div>There are 3 ways to win <strong>Keibot</strong> (pronounced <em>Key-bo</em>):</div>
 
-      1.  Get 3 beads in a row (horizontally, vertically, or diagonally)
-      2.  Capture 3 of your opponent's statues
-      3.  Place all ten of your beads on the board
-      (Actually, there is a fourth way to win, but it happens very rarely — trap your 
-      opponent so he can't move.)
+      <ol>
+        <li>Get 3 beads in a row (horizontally, vertically, or diagonally)</li>
+        <li>Capture 3 of your opponent's statues</li>
+        <li>Place all ten of your beads on the board</li>
+      </ol>
       
-      The statues move like knights in chess (an L-shaped move, two squares 
+      <p>(Actually, there is a fourth way to win, but it happens very rarely—trap your 
+      opponent so he can't move.)</p>
+      
+      <p>The statues move like knights in chess (an L-shaped move, two squares 
       horizontally or vertically and then one square perpendicularly). To move a piece, 
-      click on its square, then on the destination square.
+      click on its square, then on the destination square.</p>
       
-      Place your beads by aligning yourself with your opponent, with one square 
-      in between — a bead goes in that square.
+      <p>Place your beads by aligning yourself with your opponent, with one square 
+      in between—a bead goes in that square.</p>
       
-      Capture your opponent's statues by landing on them (except for the 
-      last to move — he's safe.  He's the one with a shield).
+      <p>Capture your opponent's statues by landing on them (except for the 
+      last to move—he's safe.  He's the one with a shield).</p>
       
-      On normal and hard levels, four idle rounds (without either a capture
-      or bead placement) result in a draw.
+      <p>On normal and hard levels, four idle rounds (without either a capture
+      or bead placement) result in a draw.</p>
       
-      On hard level, the first statue to move can't move on the second round.`
-    );
+      <div>On hard level, the first statue to move can't move on the second round.</div>
+    `);
 
     return labelText;
   }
